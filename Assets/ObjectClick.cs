5733a62786b5e87342fa42465afb7d373d116903
Jump Rice <jump@gmail.com>
2019-09-26 14:32:46 +0200

    objectClick funkar inte

---
+++ Assets/ObjectClick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectClick : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetMouseButtonDown(0))
+      {
+        print("Pressed primary button.");
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out hit, 100.0f))
+        {
+          if(hit.transform)
+          {
+            print(hit.transform.gameObject);
+          }
+        }
+      }
+    }
+
+}
