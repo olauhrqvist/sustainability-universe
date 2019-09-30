5964e715ff3418107c5702e0c1e87d46acae3d9e
Jump Rice <jump@gmail.com>
2019-09-30 12:13:25 +0200

    objectClick script update

--- Assets/ObjectClick.cs
+++ Assets/ObjectClick.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class ObjectClick : MonoBehaviour
 {
     // Update is called once per frame
-    void Update()
+    void OnGUI()
     {
       if (Input.GetMouseButtonDown(0))
       {
-        print("Pressed primary button.");
+        Debug.Log("Pressed primary button.");
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if(Physics.Raycast(ray, out hit, 100.0f))
         {
           if(hit.transform)
           {
-            print(hit.transform.gameObject.name);
+          Debug.Log(hit.transform.gameObject.name);
           }
         }
       }
