96767f82931d5bfcb0a0db4e8282fa22169d9161
calibramicke <mick_e89@hotmail.com>
2019-10-02 09:08:59 +0200

    comments on save/load implementation

--- Assets/GameState.cs
+++ Assets/GameState.cs
@@ -33,6 +33,32 @@ public class GameState : MonoBehaviour
         SceneManager.LoadScene((int)SceneNr.Pause);
     }
 
+    /* Idea Section for save game
+     
+        When you start a new game, every possible variable for animals and vegetaion will be set to null on each tile. This will
+        be saved in a 2 dimensional array or (vector if it exists) where arr[tile][variables on tile] 
+        for each arr[tile] set variables to null except the init variables.
+
+        When we're saving the game, we'll loop through each tile and set the variables for respective tile in a file.
+
+        When we're loading a game, we'll open the file and put the variables for each tile in the array. We can use a key word
+        in the file to know when we're at a new tile etc
+
+        savefile.txt
+        .
+        .
+        oak 1
+        goose 3
+        .
+        .
+        newTile //here we'll now that the tile is done, and we'll load arr[tile+1][] ...
+      
+      
+      
+     */
+
+
+
     // Update is called once per frame
     void Update()
     {
