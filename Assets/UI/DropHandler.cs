4241c63e68f26d8916de8b8cf4608ce25c123f9b
Jesper <Jesper.j.m.eriksson@gmail.com>
2019-10-03 14:13:44 +0200

    Spawn object by dragging now works
    #Progress

--- Assets/UI/DropHandler.cs
+++ Assets/UI/DropHandler.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 public class DropHandler : MonoBehaviour, IDropHandler
 {
+    public SpawnObject SpawnScript;
+    public GameObject sampleObject;
     public void OnDrop(PointerEventData eventData)
     {
         RectTransform invPanel = transform as RectTransform;
 
         if(!RectTransformUtility.RectangleContainsScreenPoint(invPanel, Input.mousePosition)){
             Debug.Log("drop");
+
+            SpawnScript.AddObject(sampleObject);
         }
 
     }
---
+++ Assets/UI/SpawnObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour
+{
+    //public GameObject sampleObject;
+
+    public void AddObject(GameObject sampleObject)
+    {
+
+        transform.rotation = Quaternion.identity;
+
+        RaycastHit hit = new RaycastHit();
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray, out hit, 1000))
+        {
+            Instantiate(sampleObject, hit.point, Quaternion.identity);
+        }
+        //Instantiate(sampleObject, hit.point, Quaternion.identity);
+        
+    }
+}
