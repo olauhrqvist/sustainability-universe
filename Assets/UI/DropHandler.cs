0590b89ef18c0605ca78f5c6434a090d05f19ab0
Jesper <Jesper.j.m.eriksson@gmail.com>
2019-10-03 12:54:31 +0200

    Drag functionality + Audiobugfix
    Much work. Wow. So fix.

---
+++ Assets/UI/DropHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class DropHandler : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        RectTransform invPanel = transform as RectTransform;
+
+        if(!RectTransformUtility.RectangleContainsScreenPoint(invPanel, Input.mousePosition)){
+            Debug.Log("drop");
+        }
+
+    }
+}
