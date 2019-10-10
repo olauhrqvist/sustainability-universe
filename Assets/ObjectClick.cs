29b2f0e6514542af870d4daad0dea15980a60206
Jump Rice <jump@gmail.com>
2019-10-10 12:35:12 +0200

    Click one tile, tile tell you how many objects on it.
    The tile now has the function to find all the obj on it.

--- Assets/ObjectClick.cs
+++ Assets/ObjectClick.cs
@@ -8,16 +8,51 @@ public class ObjectClick : MonoBehaviour
     private GameObject selectTile;
     private GameObject prevTile = null;
     private bool stay=false;
+    private bool newSelectedTile = false;
+    List<GameObject> foundObj = new List<GameObject>();
+    List<GameObject> tileObjList = new List<GameObject>();
 
     void Update()
     {
         if (stay && selectTile!=null)
         {
-            
+            findObj();
             selectTile.GetComponent<Renderer>().material.color = clickColor;
-           // selectTile.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+
+            //print(selectTile.GetComponent<Collider>().bounds.Contains(GameObject.Find("Cube").transform.position));
+            // selectTile.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
             //Debug.Log(selectTile.GetComponent<Renderer>().material.GetColor("_Color"));
+            if (newSelectedTile == true)
+            {
+                printFoundList();
+            }
+            foundObj.Clear();
+            tileObjList.Clear();
+            newSelectedTile = false;
         }
+    }
+
+    void findObj()
+    {
+        foreach (var gameObj in FindObjectsOfType(typeof(GameObject)) as GameObject[])
+        {
+            if (gameObj.name == "TemplateModel(Clone)") // I want all animal/plant obj have tag "dispalyerbal" or other
+            {
+                foundObj.Add(gameObj);
+
+            }
+        }
+
+    }
+
+    void printFoundList()
+    {
+        foreach (var obj in foundObj)
+        {
+            if (selectTile.GetComponent<Collider>().bounds.Contains(obj.transform.position))
+                tileObjList.Add(obj);
+        }
+        print("In this tile, there are: " + tileObjList.Count + " objects.");
     }
     void OnGUI()
     {
@@ -32,14 +67,17 @@ public class ObjectClick : MonoBehaviour
                 if (hit.collider.gameObject.tag== "tile")
                 {
                     //tr.Substring(0,i)
+                    
                     selectTile = hit.collider.gameObject;
                     stay = true;
-                    if (prevTile!=null && prevTile == selectTile)
+                    
+                    if (prevTile!=null && prevTile != selectTile)
                     {
-                        //Debug.Log(prevTile);
-                        //Debug.Log(selectTile); 
-
+                        newSelectedTile = true;
+                        //print("prevTile != selectTile: " + newSelectedTile);
                     }
+                    
+
                     prevTile = selectTile;
                     
                 }
