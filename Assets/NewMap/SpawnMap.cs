c9dfffd62e59b35cf2bd65a03a91d8d48e964fd1
Jump Rice <jump@gmail.com>
2019-10-10 11:33:00 +0200

    Add Input TileNumber(NxN) in spawnMap
    Default 8x8 (64) tiles

--- Assets/NewMap/SpawnMap.cs
+++ Assets/NewMap/SpawnMap.cs
@@ -11,14 +11,18 @@ public class SpawnMap : MonoBehaviour
     private float x = -15;
     private float z = -15;
     private GameObject tile;
+    public int tileNumberNxN = 8;
+
+    
 
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 4; i++)
+        
+        for (int i = 0; i < tileNumberNxN; i++)
         {
-            for (int j = 0; j < 4; j++)
+            for (int j = 0; j < tileNumberNxN; j++)
             {
 
                 //Instantiate(planeTile, new Vector3(x, 0,  z), transform.rotation);
