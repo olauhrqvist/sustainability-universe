22b7af1b2d22e46d595008a2ca59c1db01ec0a51
calibramicke <mick_e89@hotmail.com>
2019-10-17 11:03:57 +0200

    skelett för tile klassen klar

--- Assets/script for tiles/Tile.cs
+++ Assets/script for tiles/Tile.cs
@@ -5,26 +5,35 @@ using UnityEngine;
 abstract class Tile : MonoBehaviour
 {
 
-    private int[][] id; //id will be the coordinate
+    protected Pair<int, int> id; //id will be the coordinate
     protected int[] neighbours;
     protected double pH;
     protected int moisture;
-    protected int sunlight;
+    protected int sunlight = 100;
     protected int nutrition;
-    protected int freeSpace;
-    protected Tile groundType;
+    protected int freeSpace = 100;
+    protected Tile groundType; //what for?
     //private Pair<bool, base_playable> tileHashType;
 
+    public abstract void setPH(double change_val);
+    public abstract double getPH();
 
-    protected Tile()
-    {
+    public abstract void setMoisture(int change_moi);
+    public abstract int getMoisture();
 
-        freeSpace = 100;
+    public abstract void setSunlight(int change_sun);
+    public abstract int getSunlight();
 
-    }
-    public abstract void setMoisture(int new_moisture);
+    public abstract void setNutrition(int change_nut);
+    public abstract int getNutrition();
 
-    public int getMoisture() { return moisture; }
+    public abstract void setFreeSpace(int change_spa);
+    public abstract int getFreeSpace();
+
+    public abstract Pair<int,int> getCoord(); //a function that returns the x and y values
+    public abstract int[] getNeighbours(int[][] map);
+
+
 }
 
 public class Pair<T, U>
