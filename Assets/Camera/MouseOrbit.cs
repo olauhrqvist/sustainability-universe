93b64ed913a14a35546b69ebcefdaec7366d7fab
Jump Rice <jump@gmail.com>
2019-11-04 20:51:36 +0100

    Alternativ kamera
    One script, check MouseOrbit in /Camera

---
+++ Assets/Camera/MouseOrbit.cs
@@ -0,0 +1,103 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseOrbit : MonoBehaviour
+{
+
+    Vector3 offest;
+    Vector3 target;
+
+    Vector3 P1;
+
+    float distance = 15.0f;
+
+    float xSpeed = 250.0f;
+    float ySpeed = 120.0f;
+
+    float x = 0.0f;
+    float y = 0.0f;
+
+    float Speed = 40.0f;
+
+    void Start()
+    {
+
+        var angles = transform.eulerAngles;
+        x = angles.y;
+        y = angles.x;
+
+        target = new Vector3(0, 0, 0);
+    }
+
+    private void Update()
+    {
+    }
+    void LateUpdate()
+    {
+        if(Input.GetAxis("Mouse ScrollWheel")!=0)
+        {
+            float wheel = Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime * 100;
+            transform.Translate(Vector3.forward * wheel*10);
+        }
+        
+
+        if (Input.GetMouseButton(0))
+        {
+
+            x += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
+            y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+
+            var rotation = Quaternion.Euler(y, x, 0);
+            var position = rotation * new Vector3(0.0f, 0.0f, -distance) + target;
+
+            transform.rotation = rotation;
+            transform.position = position;
+
+        }
+        else if (Input.GetMouseButton(1))
+        {
+
+            float x;
+            float y;
+            x = Input.GetAxis("Mouse X");
+            y = Input.GetAxis("Mouse Y");
+            transform.Translate(new Vector3(-x, -y, 0) * Time.deltaTime * Speed);
+
+        }
+
+
+        if (Input.GetMouseButtonDown(1))
+        {
+
+            P1 = transform.position;
+
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+
+            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider)
+                {
+                    target = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+                    distance = (hit.point - transform.position).magnitude;
+                    print(hit.collider.name);
+                }
+            }
+            else
+            {
+
+                offest = transform.position - P1;
+                target = target + offest;
+                distance = (target - transform.position).magnitude;
+            }
+
+
+        }
+
+    }
+}
