a80e4786ddfbe12f4b5b702ddde031a6f0e4e9da
Jump Rice <jump@gmail.com>
2019-11-06 18:48:26 +0100

    simple popup

--- Assets/NewMap/mapClick.cs
+++ Assets/NewMap/mapClick.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class mapClick : MonoBehaviour
 {
-    public Color clickColor ;
+    public Color clickColor;
     private GameObject selectTile = null;
     private GameObject prevTile = null;
     List<GameObject> foundObj = new List<GameObject>();
@@ -12,13 +13,18 @@ public class mapClick : MonoBehaviour
     private Color planeColor;
     private Color orginColor;
     private int i = 1;
-
+    private string tileInfo;
+    private bool WindowShow = false;
+    private float windowX = 0;
+    private float windowY = 0;
+    private float windowWidth = 150;
+    private float windowHight = 150;
 
     void findObj()
     {
         foreach (var gameObj in FindObjectsOfType(typeof(GameObject)) as GameObject[])
         {
-            if (gameObj.name == "TemplateModel(Clone)" || gameObj.tag == "Animal" || gameObj.tag=="Plant") 
+            if (gameObj.name == "TemplateModel(Clone)" || gameObj.tag == "Animal" || gameObj.tag == "Plant")
             {
                 foundObj.Add(gameObj);
 
@@ -34,34 +40,54 @@ public class mapClick : MonoBehaviour
             if (selectTile.GetComponent<Collider>().bounds.Contains(obj.transform.position))
                 tileObjList.Add(obj);
         }
-        print("In this tile, there are: " + tileObjList.Count + " objects.");
+        //print("In this tile, there are: " + tileObjList.Count + " objects.");
+        tileInfo = "In this tile, there are: " + tileObjList.Count + " objects.";
+
+    }
+
+    void MyWindow(int WindowID)
+    {
+        int offset = 50;
+        if (windowHight + windowY > Screen.height)
+            windowY = windowY - windowHight;
+        if (windowWidth + windowX > Screen.width)
+            windowX = windowX - windowWidth;
+        GUI.Label(new Rect(10, 20, windowWidth, windowHight - offset), tileInfo);
+       /* if (GUI.Button(new Rect(windowWidth / 2 - windowWidth / 4, windowHight - 30, windowWidth / 2, 20), "Close"))
+        {
+            print("Close Tile Info");
+            WindowShow = false;
+        }*/
     }
     void OnGUI()
     {
-        
-        if (Input.GetMouseButtonDown(0))
-       {
-        
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if(Physics.Raycast(ray, out hit, 100))
+      
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
         {
-                
-                if (hit.collider.gameObject.tag== "tile")
+
+
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                if (hit.collider.gameObject.tag == "tile")
                 {
                     //tr.Substring(0,i)
+                    WindowShow = true;
+                    windowX = Input.mousePosition.x;
+                    windowY = Screen.height - Input.mousePosition.y;
                     planeColor = hit.collider.gameObject.GetComponent<Renderer>().material.GetColor("_Color");
                     if (i == 1)
                     {
                         orginColor = planeColor;
                         i = 0;
                     }
-                    
+
                     selectTile = hit.collider.gameObject;
-                    
-                    if (prevTile !=null && prevTile != selectTile)
+
+                    if (prevTile != null && prevTile != selectTile)
                     {
-                     
+
                         selectTile.GetComponent<Renderer>().material.color = clickColor;
                         prevTile.GetComponent<Renderer>().material.color = orginColor;
                     }
@@ -74,24 +100,22 @@ public class mapClick : MonoBehaviour
                 }
                 else if (hit.collider.gameObject.tag == "Plant")
                 {
+                    WindowShow = false;
                     print("hit trees");
                     if (selectTile != null)
                         selectTile.GetComponent<Renderer>().material.color = orginColor;
                 }
                 else
                 {
+                    WindowShow = false;
                     print("hit other things");
-                    if(selectTile!=null)
+                    if (selectTile != null)
                         selectTile.GetComponent<Renderer>().material.color = orginColor;
                 }
-                
+
+            }
         }
-      }
+        if (WindowShow)
+            GUI.Window(0, new Rect(windowX, windowY, windowWidth, windowHight), MyWindow, "   ");
     }
-
-    
-
-
-
-
 }
