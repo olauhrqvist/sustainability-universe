9e1fe835677d86a82d993f0690c233d66055c4d4
johankristofferlarsson <37417924+johankristofferlarsson@users.noreply.github.com>
2019-11-06 10:19:26 +0100

    Added simple global stopwatch

---
+++ Assets/Clock/ClockScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+public class ClockScript : MonoBehaviour
+{
+    private Text textClock;
+    public float seconds, minutes;
+    void Awake()
+    {
+        textClock = GetComponent<Text>();
+    }
+    void Update()
+    {
+        minutes = (int)(Time.time / 60f);
+        seconds = (int)(Time.time % 60f);
+        textClock.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+
+    }
+}
