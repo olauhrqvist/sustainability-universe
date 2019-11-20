f76c4d2307de2b8f18a4a3af26af466b48ea0422
Jump Rice <jump@gmail.com>
2019-11-20 12:33:13 +0100

    change some value

--- Assets/Playables/Tree_Script.cs
+++ Assets/Playables/Tree_Script.cs
@@ -20,13 +20,13 @@ public class Tree_Script : Vegetation
                                                     0, //hierarchy =0 ? not sure if it's 0 
                                                     ID, // what is ID for a tree ?
                                                     mesh, //?
-                                                    range, // ? 
-                                                    space, // ?
+                                                    1, // range = 1 tile
+                                                    10, // space = 10 ? 
                                                     enviroment,//?
                                                     forestid, // set in tile script
                                                     growthtime, // dynamic or fixed? if fixed value, set here.
                                                     species, // name of tree? set in tile script
-                                                    SunlightCost, //not sure
+                                                    SunlightCost, //ignore 
                                                     NutritionalCost)  // not sure, each tree cost the same ? or based on species?
     {
 
