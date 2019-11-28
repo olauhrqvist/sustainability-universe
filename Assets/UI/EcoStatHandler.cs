389c5e8aa6820cbddf2b5e5b5a797d6ccee891a7
Jesper <Jesper.j.m.eriksson@gmail.com>
2019-11-29 00:05:06 +0100

    Added animal view
    should now be able to see where animals are.
    fixed a bug with placing animals on trees.
    added textures to trees in menu.

--- Assets/UI/EcoStatHandler.cs
+++ Assets/UI/EcoStatHandler.cs
@@ -8,8 +8,9 @@ public class EcoStatHandler : MonoBehaviour
     private Global_Database globalDatabase;
     private TileClass tileclass;
     static private Color DefaultColor = new Color(0, 102, 0);
+    static private Color MarkColor = new Color(0, 0, 1, 100);
     Queue<Action> ActionQueue = new Queue<Action>();    // Used for toggling UI changes
-
+    private string LastAction;
     // Color tiles based on ground type
     public void DisplaypH(bool active)
     {
@@ -18,43 +19,159 @@ public class EcoStatHandler : MonoBehaviour
         {
             Action action = ActionQueue.Dequeue();
             action();
-        }
-        else
-        {
-            List<TileClass> tiles = GameObject.Find("SpawnMap").GetComponent<SpawnMap>().tiles;
-            if (!active)
+            if (LastAction == "pH")
             {
-                foreach (var tile in tiles)
-                {
-
-
-                    if (tile.Groundtype == "brownearth")
-                    {
-                        Color color = new Color(51, 102, 0);
-                        tile.tileGameObject.GetComponent<Renderer>().material.color = color;
-                    }
-
-                    else if (tile.Groundtype == "podzol")
-                    {
-                        Color color = new Color(0, 102, 0);
-                        tile.tileGameObject.GetComponent<Renderer>().material.color = color;
-                    }
-                }
-                // Add it to the queue so that next EcoStat action will reverse this
-                ActionQueue.Enqueue(() => DisplaypH(true));
+                LastAction = "";
+                active = true;
             }
-            else
+        }
+
+        List<TileClass> tiles = GameObject.Find("SpawnMap").GetComponent<SpawnMap>().tiles;
+        if (!active)
+        {
+            foreach (var tile in tiles)
             {
-                // Reverse the coloring
-                foreach (var tile in tiles)
+                if (tile.Groundtype == "brownearth")
                 {
                     tile.tileGameObject.GetComponent<Renderer>().material.color = DefaultColor;
                 }
+
+                else if (tile.Groundtype == "podzol")
+                {
+                    Color color = Color.yellow;
+                    tile.tileGameObject.GetComponent<Renderer>().material.color = color;
+                }
             }
+            // Add it to the queue so that next EcoStat action will reverse this
+            ActionQueue.Enqueue(() => ClearMap());
+            LastAction = "pH";
         }
     }
+
+    // Default change tile color. used since function only can take 1 argument
     public void DisplayAnimalLocation(string Animal)
     {
-        return;
+        DisplayAnimalHandler(false, Animal);
+    }
+    public void DisplayAnimalHandler(bool active, string Animal)
+    {
+        // Empties queue if it exists
+        if (ActionQueue.Count != 0)
+        {
+            Action action = ActionQueue.Dequeue();
+            action();
+            if(LastAction == Animal)
+            {
+                LastAction = "";
+                active = true;
+            }
+        }
+
+        List<TileClass> tiles = GameObject.Find("SpawnMap").GetComponent<SpawnMap>().tiles;
+        if (!active)
+        {
+            globalDatabase = GameObject.Find("SpawnMap").GetComponent<SpawnMap>().globalDatabase;
+            string temp = "";
+            string animaltype = GameObject.Find(Animal).GetComponent<Base_Playable>().GetBaseType();
+            
+            switch (animaltype) // Searches different databases depending on type // can be optimized
+            {
+                case "Carnivore":
+                    List<CarnivoreInfo> ElementListCar = globalDatabase.CarnivoreList;
+                    foreach (var A in ElementListCar)        // Find all animals that match the string
+                    {
+                        if (A.name == Animal)
+                        {
+                            foreach (var T in tiles)        // Search for tile
+                            {
+                                temp += T.x;
+                                temp += T.y;
+                                //Debug.Log(A.TilePosition+" =? "+ temp);
+
+                                if (A.TilePosition == temp)     // If Tile matches animal location
+                                {
+                                    T.tileGameObject.GetComponent<Renderer>().material.color = MarkColor;//material.color = color;
+                                }
+                                temp = "";
+                            }
+                        }
+                    }
+                    break;
+                case "Herbivore":
+                    List<HerbivoreInfo> ElementListHer = globalDatabase.HerbivoreList;
+                    foreach (var A in ElementListHer)        // Find all animals that match the string
+                    {
+                        if (A.name == Animal)
+                        {
+                            foreach (var T in tiles)        // Search for tile
+                            {
+                                temp += T.x;
+                                temp += T.y;
+
+                                if (A.TilePosition == temp)     // If Tile matches animal location
+                                {
+                                    T.tileGameObject.GetComponent<Renderer>().material.color = MarkColor;//material.color = color;
+                                }
+                                temp = "";
+                            }
+                        }
+                    }
+                    break;
+                case "Omnivore":
+                    List<OmnivoreInfo> ElementListOmn = globalDatabase.OmnivoreList;
+                    foreach (var A in ElementListOmn)        // Find all animals that match the string
+                    {
+                        if (A.name == Animal)
+                        {
+                            foreach (var T in tiles)        // Search for tile
+                            {
+                                temp += T.x;
+                                temp += T.y;
+
+                                if (A.TilePosition == temp)     // If Tile matches animal location
+                                {
+                                    T.tileGameObject.GetComponent<Renderer>().material.color = MarkColor;//material.color = color;
+                                }
+                                temp = "";
+                            }
+                        }
+                    }
+                    break;
+                case "Tree":
+                    List<TreeTypeInfo> ElementListTree = globalDatabase.TreeTypeList;
+                    foreach (var A in ElementListTree)        // Find all animals that match the string
+                    {
+                        if (A.name == Animal)
+                        {
+                            foreach (var T in tiles)        // Search for tile
+                            {
+                                temp += T.x;
+                                temp += T.y;
+
+                                if (A.TilePosition == temp)     // If Tile matches animal location
+                                {
+                                    T.tileGameObject.GetComponent<Renderer>().material.color = MarkColor;//material.color = color;
+                                }
+                                temp = "";
+                            }
+                        }
+                    }
+                    break;
+                default:
+                    Debug.Log("Animal type not found");
+                    break;
+            }
+            ActionQueue.Enqueue(() => ClearMap());
+            LastAction = Animal;
+        }
+    }
+    private void ClearMap()
+    {
+        List<TileClass> tiles = GameObject.Find("SpawnMap").GetComponent<SpawnMap>().tiles;
+
+        foreach (var tile in tiles)
+        {
+            tile.tileGameObject.GetComponent<Renderer>().material.color = DefaultColor;
+        }
     }
 }
