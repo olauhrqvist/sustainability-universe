eaa4ba2a9ec46701188176c07966dd58e763ff2a
Jesper <Jesper.j.m.eriksson@gmail.com>
2019-12-03 12:05:23 +0100

    Implemented rewardsystem (not balanced)

--- Assets/RewardSystem/YearCounter.cs
+++ Assets/RewardSystem/YearCounter.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class YearCounter : MonoBehaviour
 {
@@ -9,14 +10,20 @@ public class YearCounter : MonoBehaviour
     public Global_Database Database;
     public BalanceWorld bw;
 
-
     //variable
 
     private int Year = 0;
     private float Counter = 10f;
 
-    //------------------------------------------------------------------------------\\
+    public GameObject text;
 
+    //------------------------------------------------------------------------------\\
+   public void Start()
+    {
+        other.currency = 100;
+        text.GetComponent<Text>().text = other.currency + " KR";
+        //rewardsystem = GameObject.Find("SpawnMap").GetComponent<RewardSystem>();
+    }
     public void Update()
     {
 
@@ -26,8 +33,10 @@ public class YearCounter : MonoBehaviour
             Counter += Counter;          
             other.Calculate();
 
-            bw.YearUpdate();//this is causing a nullreference error.
+            //bw.YearUpdate();//this is causing a nullreference error.
+
+            text.GetComponent<Text>().text = other.currency + " KR";
         }
-        
+
     }
 }
--- Assets/UI/DropHandler.cs
+++ Assets/UI/DropHandler.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using System;
+using UnityEngine.UI;
+
 public class DropHandler : MonoBehaviour, IDropHandler
 {
     public GameObject Spawnable;
     string Name, Type;
     private List<TileClass> tiles;
     private Global_Database globalDatabase;
+    private RewardSystem rewardsystem;
+    private GameObject text;
 
-
-
-
+    public void Start()
+    {
+        rewardsystem = GameObject.Find("EventSystem").GetComponent<YearCounter>().other;
+        text = GameObject.Find("EventSystem").GetComponent<YearCounter>().text;
+    }
 
     public void OnDrop(PointerEventData eventData)
     {
@@ -59,16 +65,34 @@ public class DropHandler : MonoBehaviour, IDropHandler
                 switch (sampleObject)
                 {
                     case "Spruce":
+                        if (rewardsystem.currency < 10)
+                        {
+                            Debug.Log("Unsufficient funds!"); //change to break when ready
+                            break;
+                        }
+                        rewardsystem.currency -= 10f;
                         globalDatabase.AddSpruce(name, Spawnable);
                         //print("Tile " + name + " and SpruceList count is: " + globalDatabase.SpruceList.Count);
                         break;
 
                     case "Birch":
+                        if (rewardsystem.currency < 15)
+                        {
+                            Debug.Log("Unsufficient funds!"); //change to break when ready
+                            break;
+                        }
+                        rewardsystem.currency -= 15f;
                         globalDatabase.AddBirch(name, Spawnable);
                         //print("Tile " + name + " and BirchList count is: " + globalDatabase.BirchList.Count);
                         break;
 
                     case "Beech":
+                        if (rewardsystem.currency < 20)
+                        {
+                            Debug.Log("Unsufficient funds!"); //change to break when ready
+                            break;
+                        }
+                        rewardsystem.currency -= 20f;
                         globalDatabase.AddBeech(name, Spawnable);
                         //print("Tile " + name + " and BeechList count is: " + globalDatabase.BeechList.Count);
                         break;
@@ -83,21 +107,45 @@ public class DropHandler : MonoBehaviour, IDropHandler
                 switch (sampleObject)
                 {
                     case "Shrew":
+                        if (rewardsystem.currency < 10)
+                        {
+                            Debug.Log("Unsufficient funds!"); //change to break when ready
+                            break;
+                        }
+                        rewardsystem.currency -= 10f;
                         globalDatabase.AddShrew(name, Spawnable);
                         print("Tile " + name + " and ShrewList count is: " + globalDatabase.ShrewList.Count);
                         break;
 
                     case "Weasel":
+                        if (rewardsystem.currency < 20)
+                        {
+                            Debug.Log("Unsufficient funds!"); //change to break when ready
+                            break;
+                        }
+                        rewardsystem.currency -= 20f;
                         globalDatabase.AddWeasel(name, Spawnable);
                         print("Tile " + name + " and WeaselList count is: " + globalDatabase.WeaselList.Count);
                         break;
 
                     case "Fox":
+                        if (rewardsystem.currency < 40)
+                        {
+                            Debug.Log("Unsufficient funds!"); //change to break when ready
+                            break;
+                        }
+                        rewardsystem.currency -= 40f;
                         globalDatabase.AddFox(name, Spawnable);
                         print("Tile " + name + " and FoxList count is: " + globalDatabase.FoxList.Count);
                         break;
 
                     case "Wolf":
+                        if (rewardsystem.currency < 80)
+                        {
+                            Debug.Log("Unsufficient funds!"); //change to break when ready
+                            break;
+                        }
+                        rewardsystem.currency -= 80f;
                         globalDatabase.AddWolf(name, Spawnable);
                         print("Tile " + name + " and WolfList count is: " + globalDatabase.WolfList.Count);
                         break;
@@ -112,21 +160,45 @@ public class DropHandler : MonoBehaviour, IDropHandler
                 switch (sampleObject)
                 {
                     case "Mouse":
+                        if (rewardsystem.currency < 10)
+                        {
+                            Debug.Log("Unsufficient funds!"); //change to break when ready
+                            break;
+                        }
+                        rewardsystem.currency -= 10f;
                         globalDatabase.AddMouse(name, Spawnable);
                         print("Tile " + name + " and MouseList count is: " + globalDatabase.MouseList.Count);
                         break;
 
                     case "Hare":
+                        if (rewardsystem.currency < 20)
+                        {
+                            Debug.Log("Unsufficient funds!"); //change to break when ready
+                            break;
+                        }
+                        rewardsystem.currency -= 20f;
                         globalDatabase.AddHare(name, Spawnable);
                         print("Tile " + name + " and HareList count is: " + globalDatabase.HareList.Count);
                         break;
 
                     case "RoeDeer":
+                        if (rewardsystem.currency < 40)
+                        {
+                            Debug.Log("Unsufficient funds!"); //change to break when ready
+                            break;
+                        }
+                        rewardsystem.currency -= 40f;
                         globalDatabase.AddDeer(name, Spawnable);
                         print("Tile " + name + " and DeerList count is: " + globalDatabase.DeerList.Count);
                         break;
 
                     case "Moose":
+                        if (rewardsystem.currency < 80)
+                        {
+                            Debug.Log("Unsufficient funds!"); //change to break when ready
+                            break;
+                        }
+                        rewardsystem.currency -= 80f;
                         globalDatabase.AddMoose(name, Spawnable);
                         print("Tile " + name + " and MooseList count is: " + globalDatabase.MooseList.Count);
                         break;
@@ -141,21 +213,45 @@ public class DropHandler : MonoBehaviour, IDropHandler
                 switch (sampleObject)
                 {
                     case "Squirrel":
+                        if (rewardsystem.currency < 10)
+                        {
+                            Debug.Log("Unsufficient funds!"); //change to break when ready
+                            break;
+                        }
+                        rewardsystem.currency -= 10f;
                         globalDatabase.AddSquirrel(name, Spawnable);
                         print("Tile " + name + " and SquirrleList count is: " + globalDatabase.SquirrelList.Count);
                         break;
 
                     case "Rat":
+                        if (rewardsystem.currency < 15)
+                        {
+                            Debug.Log("Unsufficient funds!"); //change to break when ready
+                            break;
+                        }
+                        rewardsystem.currency -= 15f;
                         globalDatabase.AddRat(name, Spawnable);
                         print("Tile " + name + " and RatList count is: " + globalDatabase.RatList.Count);
                         break;
 
                     case "Boar":
+                        if (rewardsystem.currency < 40)
+                        {
+                            Debug.Log("Unsufficient funds!"); //change to break when ready
+                            break;
+                        }
+                        rewardsystem.currency -= 40f;
                         globalDatabase.AddBoar(name, Spawnable);
                         print("Tile " + name + " and BoarList count is: " + globalDatabase.BoarList.Count);
                         break;
 
                     case "BrownBear":
+                        if (rewardsystem.currency < 95)
+                        {
+                            Debug.Log("Unsufficient funds!"); //change to break when ready
+                            break;
+                        }
+                        rewardsystem.currency -= 95f;
                         globalDatabase.AddBear(name, Spawnable);
                         print("Tile " + name + " and BrownBearList count is: " + globalDatabase.BrownBearList.Count);
                         break;
@@ -171,5 +267,6 @@ public class DropHandler : MonoBehaviour, IDropHandler
                 break;
 
         }
+        text.GetComponent<Text>().text = rewardsystem.currency + " KR";
     }
 }
