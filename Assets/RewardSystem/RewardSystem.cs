eebb5b1c72254af496c6ce19ef0b0c66d03e7e9f
calibramicke <mick_e89@hotmail.com>
2019-12-04 16:03:19 +0100

    ...

--- Assets/RewardSystem/RewardSystem.cs
+++ Assets/RewardSystem/RewardSystem.cs
@@ -1,30 +1,59 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 
 public class RewardSystem : MonoBehaviour
 {
-  public float currency = 100;
+    public float currency = 100;
 
-  public int TotalReward;
-  private GameObject MYgameObject;
+    public float TotalReward;
+    public float AnimalTotalReward;
+    public float TreeTotalReward;
 
-  public List<TileClass> listan = new List<TileClass>();
+    private GameObject MYgameObject;
+
+    public List<TileClass> listan = new List<TileClass>();
 
 
 
 
-  public void Calculate(int Happiness)
-  {
-    AddCurrency(Happiness);
-  }
+    public void Calculate()
+    {    
+        listan = GameObject.Find("SpawnMap").GetComponent<SpawnMap>().Getlist();
+        NewCheck();
 
-  public void AddCurrency(int Happy)
-  {
-    currency += 10 * (float)Happy; //exempel
-    print(currency + "   " + Happy);
-  }
+        foreach(TileClass i in listan)
+        {
+           CalculateHappines(i);
+        }
+
+         TotalReward = CaluclateTotalHappines(AnimalTotalReward, TreeTotalReward, listan.Count);
+
+    }
+
+    public void NewCheck()
+    {
+        TotalReward = 0f;
+        AnimalTotalReward = 0f;
+        TreeTotalReward = 0f;
+    }
+
+    public void CalculateHappines(TileClass i)
+    {
+        AnimalTotalReward += i.AnimalHappiness;
+        TreeTotalReward += i.TreeHappiness;
+    }
+
+    public float CaluclateTotalHappines(float AnimalTotalReward, float TreeTotalReward,  int tileSize)
+    {
+        return ((AnimalTotalReward + TreeTotalReward) / (tileSize/2));
+    }
+
+    public void AddCurrency(float TotalReward)
+    {
+        currency += 1000f * TotalReward; //exempel
+    }
 
 
 
---
+++ Assets/UI/NotificationScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NotificationScript : MonoBehaviour
+{
+    public GameObject prefab;
+    public void AddToStack()
+    {
+        var parent = GameObject.Find("NotificationsContent");
+        GameObject createImage = Instantiate(prefab) as GameObject;
+        createImage.transform.SetParent(parent.transform, false);
+        createImage.transform.SetAsFirstSibling();
+        createImage.GetComponentInChildren<Text>().text = "aye lemonlemonlemonlemonlemonlemonlemon";
+    }
+
+}
