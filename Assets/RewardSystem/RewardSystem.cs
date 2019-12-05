fdaf4059d2ac027ee2f31437a2392e606f7c3a92
Roxxxers <drini007@live.se>
2019-12-05 09:45:36 +0100

    camera borders and RS fixed

--- Assets/RewardSystem/RewardSystem.cs
+++ Assets/RewardSystem/RewardSystem.cs
@@ -5,55 +5,29 @@ using UnityEngine;
 
 public class RewardSystem : MonoBehaviour
 {
-    public float currency = 100;
+  public float currency = 100;
 
-    public float TotalReward;
-    public float AnimalTotalReward;
-    public float TreeTotalReward;
+  public float TotalReward;
+  public float AnimalTotalReward;
+  public float TreeTotalReward;
 
-    private GameObject MYgameObject;
+  private GameObject MYgameObject;
 
-    public List<TileClass> listan = new List<TileClass>();
-
-
+  public List<TileClass> listan = new List<TileClass>();
 
 
-    public void Calculate()
-    {    
-        listan = GameObject.Find("SpawnMap").GetComponent<SpawnMap>().Getlist();
-        NewCheck();
 
-        foreach(TileClass i in listan)
-        {
-           CalculateHappines(i);
-        }
 
-         TotalReward = CaluclateTotalHappines(AnimalTotalReward, TreeTotalReward, listan.Count);
+  public void Calculate(int Happiness)
+  {
+    AddCurrency(Happiness);
+  }
+  public void AddCurrency(int Reward)
+  {
 
-    }
-
-    public void NewCheck()
-    {
-        TotalReward = 0f;
-        AnimalTotalReward = 0f;
-        TreeTotalReward = 0f;
-    }
-
-    public void CalculateHappines(TileClass i)
-    {
-        AnimalTotalReward += i.AnimalHappiness;
-        TreeTotalReward += i.TreeHappiness;
-    }
-
-    public float CaluclateTotalHappines(float AnimalTotalReward, float TreeTotalReward,  int tileSize)
-    {
-        return ((AnimalTotalReward + TreeTotalReward) / (tileSize/2));
-    }
-
-    public void AddCurrency(float TotalReward)
-    {
-        currency += 1000f * TotalReward; //exempel
-    }
+    currency += 10f * (float)Reward; //exempel
+    print(Reward + "     " + currency);
+  }
 
 
 
