60e3a3a207d56cc1c6b8ab2af885fd091dbde81b
Jump Rice <jump@gmail.com>
2019-12-09 19:53:01 +0100

    clickMap doc

--- Assets/NewMap/mapClick.cs
+++ Assets/NewMap/mapClick.cs
@@ -11,13 +11,12 @@ public class mapClick : MonoBehaviour
     private Color planeColor;
     private Color orginColor;
     private int i = 1;
-    private string animalInfo;
     private string tileInfo;
-    private bool WindowShow = false;
-    private float windowX = 0;
-    private float windowY = 0;
-    private float windowWidth = 220;//200
-    private float windowHight = 195;//175
+    private bool WindowShow = false; // bool of tile info window
+    private float windowX = 0; //tile info window position X
+    private float windowY = 0; //tile info window position Y
+    private float windowWidth = 230;//tile info window width
+    private float windowHight = 200;//tile info window Hight
     private GameObject closeButton;
     private Global_Database globalDatabase;// = new Global_Database();
 
@@ -38,10 +37,7 @@ public class mapClick : MonoBehaviour
         closeButton.GetComponent<Image>().color = Color.clear;
         closeButton.GetComponentInChildren<Text>().text = "";
         closeButton.SetActive(false);
-
-
     }
-
     private void Update()
     {
         if (WindowShow)
@@ -52,7 +48,7 @@ public class mapClick : MonoBehaviour
 
     void updateTileInfo()
     {
-        // Fetch Database
+        // Fetch Database and caculate population for each animal type. 
         string animalInfo = "";
           globalDatabase = GameObject.Find("SpawnMap").GetComponent<SpawnMap>().globalDatabase;
         int herbivores = globalDatabase.calculateHerbivores(selectTile.GetComponent<Collider>().name);
@@ -67,28 +63,20 @@ public class mapClick : MonoBehaviour
         int rat = globalDatabase.calculateRat(selectTile.GetComponent<Collider>().name);
         int brownBear = globalDatabase.calculateBrownBear(selectTile.GetComponent<Collider>().name);
         int mouse = globalDatabase.calculateMouse(selectTile.GetComponent<Collider>().name);
-
-
-        /*var mouseTest = globalDatabase.MouseList.Find(x => x.TilePosition == selectTile.GetComponent<Collider>().name);
-        if (mouseTest != null)
-        {
-          animalInfo += "Mice: " + mouseTest.population + "\r\n";
-        }*/
-
-
         int hare = globalDatabase.calculateHare(selectTile.GetComponent<Collider>().name);
         int moose = globalDatabase.calculateMoose(selectTile.GetComponent<Collider>().name);
         int deer = globalDatabase.calculateDeer(selectTile.GetComponent<Collider>().name);
 
+        // Append animal polulation to the tile info
         // Herbivores
         if (globalDatabase.calculateHerbivores(selectTile.GetComponent<Collider>().name) > 0)
           animalInfo += "Herbivores" + "\r\n";
         if (mouse > 0)
-            animalInfo += "Mice: " + mouse + "\r\n";
+            animalInfo += "Mice: " + mouse + "  ";
         if (hare != 0)
-            animalInfo += "Hares: " + hare + "\r\n";
+            animalInfo += "Hares: " + hare + "  ";
         if (deer != 0)
-            animalInfo += "Deer: " + deer + "\r\n";
+            animalInfo += "Deer: " + deer + "  ";
         if (moose != 0)
             animalInfo += "Moose " + moose + "\r\n";
 
@@ -96,34 +84,31 @@ public class mapClick : MonoBehaviour
         if (globalDatabase.calculateOmnivores(selectTile.GetComponent<Collider>().name) > 0)
           animalInfo += "\r\n" + "Omnivores" + "\r\n";
         if (squirrel != 0)
-            animalInfo += "Squirrels: " + squirrel + "\r\n";
+            animalInfo += "Squirrels: " + squirrel + "  ";
         if (rat != 0)
-            animalInfo += "Rats: " + rat +  "\r\n";
+            animalInfo += "Rats: " + rat +  "  ";
         if (boar != 0)
-            animalInfo += "Boar: " + boar +  "\r\n";
+            animalInfo += "Boar: " + boar +  "  ";
         if (brownBear != 0)
-            animalInfo += "Brown Bears: " + brownBear +  "\r\n";
+            animalInfo += "Brown Bears: " + brownBear +  "  ";
 
         // Carnivores
         if (globalDatabase.calculateCarnivores(selectTile.GetComponent<Collider>().name) > 0)
           animalInfo += "\r\n" + "Carnivores" + "\r\n";
         if (shrew != 0)
-            animalInfo += "Shrews: " + shrew + "\r\n";
+            animalInfo += "Shrews: " + shrew + "  ";
         if (fox != 0)
-            animalInfo += "Foxes: " + fox + "\r\n";
+            animalInfo += "Foxes: " + fox + "  ";
         if (weasel != 0)
-            animalInfo += "Weasels: " + weasel + "\r\n";
+            animalInfo += "Weasels: " + weasel + "  ";
         if (wolf != 0)
-            animalInfo += "Wolves: " + wolf + "\r\n";
+            animalInfo += "Wolves: " + wolf + "  ";
 
-
+        // Caculate vegetation information for the tile
         List<TileClass> tiles = GameObject.Find("SpawnMap").GetComponent<SpawnMap>().tiles;
-        //double meat = tiles.Find(x => x.name == selectTile.GetComponent<Collider>().name).meatOnTile;
-        List<double> meatlist = tiles.Find(x => x.name == selectTile.GetComponent<Collider>().name).foodHierarchy;
         double vegetation = tiles.Find(x => x.name == selectTile.GetComponent<Collider>().name).vegetationOnTile;
         int treenumber = tiles.Find(x => x.name == selectTile.GetComponent<Collider>().name).tileTrees.Count;
-
-
+        //Add vegetation status to tile info
         string vegetationStatus;
         if (vegetation < 0)
           vegetationStatus = "Not enough food";
@@ -133,50 +118,43 @@ public class mapClick : MonoBehaviour
           vegetationStatus = "Medium";
         else
           vegetationStatus = "High";
-
-        tileInfo = "Tile: " + selectTile.GetComponent<Collider>().name + "\r\n"
+        // The whole text of tile info:
+        tileInfo = "Tile: " + selectTile.GetComponent<Collider>().name + "  "
         + "Trees:     " + (treenumber) + "\r\n"
         + "Vegetation: " + vegetationStatus + " (" + vegetation + ")" + "\r\n"
-        + animalInfo + "\r\n";
+        + animalInfo ;
         //+ "Total food: " + "\r\n";
     }
 
     void MyWindow(int WindowID)
     {
-        int offset = 50;
+        int offset = 50; // Set the offset in case the window is out of the screen. 
         if (windowHight + windowY > Screen.height)
             windowY = windowY - windowHight;
         if (windowWidth + windowX > Screen.width)
             windowX = windowX - windowWidth;
 
-
+        //Tile info shown in the label here. Label is in the window. 
         GUI.Label(new Rect(10, 20, windowWidth-15, windowHight - offset), tileInfo);
-
-        ////// Invisible(tranparent) ui button behind the gui button so the ray light can not go through the gui close button.
-       // closeButton.SetActive(true);
-        Vector3 pos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
-        closeButton.GetComponent<RectTransform>().sizeDelta = new Vector2(windowWidth / 3, 20);
-        closeButton.transform.position = pos;
-        //////
-        ///
+        //Close button in tile info window
         if (GUI.Button(new Rect(windowWidth / 2 - windowWidth / 4, windowHight - 30, windowWidth / 2, 20), "Close"))
-        {
-
-            //print("Close Tile Info");
-            WindowShow = false;
-            closeButton.SetActive(false);
+        { 
+            WindowShow = false; // if close is pressed, the window show is false, then user can select another tile. 
             if (selectTile != null)
-                selectTile.GetComponent<Renderer>().material.color = orginColor;
+                selectTile.GetComponent<Renderer>().material.color = orginColor; // change the tile color to orgin color.
         }
 
     }
     void OnGUI()
     {
+        // Mouse click event. 
+        // Check the tile click only when tile info is closed. 
         if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject() && WindowShow == false)
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit, 200f, LayerMask.GetMask("MapGround")))
+            // Only check the raycast hit to the tile layer which called "MapGround". So the tile click will not hit the trees or others. 
+            if (Physics.Raycast(ray, out hit, 200f, LayerMask.GetMask("MapGround"))) // Tiles have the layer "MapGround".
             {
                 if (hit.collider.gameObject.tag == "tile")
                 {
@@ -184,7 +162,6 @@ public class mapClick : MonoBehaviour
                     WindowShow = true;
                     windowX = Input.mousePosition.x;
                     windowY = Screen.height - Input.mousePosition.y;
-
                     planeColor = hit.collider.gameObject.GetComponent<Renderer>().material.GetColor("_Color");
                     selectTile = hit.collider.gameObject;
                     if (i == 1)
@@ -193,19 +170,14 @@ public class mapClick : MonoBehaviour
                         i = 0;
                         selectTile.GetComponent<Renderer>().material.color = clickColor;
                     }
-
-
                     selectTile.GetComponent<Renderer>().material.color = clickColor;
-
-
-
                 }
 
             }
         }
         if (WindowShow)
         {
-           // updateTileInfo();
+           //If a tile is selected, show the tile info window. 
             GUI.Window(0, new Rect(windowX, windowY, windowWidth, windowHight), MyWindow, "   ");
         }
 
